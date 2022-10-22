ae9c85234f22f61a8b2c54130d79b06126a82c7c
Able <ablekilinda@gmail.com>
2022-10-22 10:58:02 +0300

    lotan

--- backend/signup-inc.php
+++ backend/signup-inc.php
@@ -13,6 +13,31 @@
    $passwordRepeat = $_POST['pwd-repeat'];
 //error handler checking if the texx boxes are empty
    if(empty($username) || empty($email) || empty($password) || empty($passwordRepeat)){
-    header("Location: ../signup.php?error=empyfields$uid".$username."&mail".$email."$");
+    header("Location: ../signup.php?error=empyfields$uid=".$username."&mail=".$email);
+    exit();
+   }
+
+   //check valid email and a valid username
+   elseif(!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)){
+    header("Location: ../signup.php?error=invalidmailuid");
+    exit();
+   }
+
+   //check if email is valid
+   elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    header("Location: ../signup.php?error=invalidmail$uid=".$username);
+    exit();
+   }
+   
+   //check for valid chracters
+   elseif(!preg_match("/^[a-zA-Z0-9]*$/", $username)){
+    header("Location: ../signup.php?error=invaliduid$mail=".$email);
+    exit();
+   }
+
+   //check if passwords match
+   elseif($password !== $passwordRepeat){
+    header("Location: ../signup.php?error=passwordcheck$uid=".$username."&mail".$email);
+    exit();
    }
  }
