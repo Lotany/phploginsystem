59d4c5fc237b76ff301577ac7c0aa84134ec2369
Able <ablekilinda@gmail.com>
2022-10-22 15:40:50 +0300

    adding dashboard

---
+++ backend/login-inc.php
@@ -0,0 +1,62 @@
+<?php
+   
+
+if (isset($_POST['login-submit'])){
+    require 'dbConnect.php';
+    $mailuid = $_POST['mailuid'];
+    $password =$_POST['pwd'];
+
+    if (empty($mailuid) || empty($password)){
+        header("Location: ../index.php?error=emptyfields");
+        exit();
+    }
+    else{
+     $sql = "SELECT * FROM users WHERE uidUsers=? OR  emailUsers=?;";
+     //initialize the stmt
+     $stmt = mysqli_stmt_init($conn);
+
+     //check if it works in a database
+     if(!mysqli_stmt_prepare($stmt,$sql)){
+        header("Location: ../index.php?error=sqlerror");
+        exit();
+     }
+     else{
+        mysqli_stmt_bind_param($stmt,"ss", $mailuid, $password);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if($row = mysqli_fetch_assoc($result)){
+        
+            //harsh the password the user entered and check wether they match with the harshed one in the database
+            $passwordCheck = password_verify($password, $row['pwdUsers']);
+            if ($passwordCheck== false){
+                header("Location: ../index.php?error=wrongpassword");
+                exit();
+            }
+            elseif($passwordCheck == true){
+                session_start();
+                $_SESSION['userId']=$row['idUsers'];
+                $_SESSION['userUid']=$row['uidUsers'];
+            }
+
+            else{
+                header("Location: ../index.php?error=wrongpassword");
+                exit();
+            }
+        }
+        else{
+            header("Location: ../index.php?error=nouser");
+            exit();
+        }
+     }
+
+    }
+    
+
+
+
+}
+ //send users back to index page if they access signup page
+ else{
+    header("Location: ../index.php");
+                exit();
+ }
--- header.php
+++ header.php
@@ -19,7 +19,7 @@
                     <li><a href="#">Contact</a></li>
                 </ul>
                 <div>
-                    <form action="backend/login.php" method="post">
+                    <form action="backend/login-inc.php" method="post">
                         <input type="text" name="mailuid" id="" placeholder="Username/E-mail..">
                         <input type="password" name="pwd" placeholder="Password..">
                         <button name="login-submit" type="submit">Login</button>
